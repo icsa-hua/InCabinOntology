a27331038f6f33f11edfe8b29251bd7e253a6d3c
dgeorgiadis <dgeorgiadis@hua.gr>
2024-09-03 15:49:12 +0300

    Fix: Organized into dirs, easier conversion and merge and new data csv

--- scripts/conversion.py
+++ scripts/conversion.py
@@ -2,15 +2,39 @@ from rdflib import Graph, URIRef, Literal, Namespace, RDF, RDFS, OWL
 from rdflib.namespace import XSD 
 import csv
 import logging 
-
+import os
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-logging.info("Starting conversion of CSV to OWL...")
+logger.info("Starting conversion of CSV to OWL...")
 
 #Create a new graph which holds the information about the csv ontology 
 graph = Graph() 
+
+#Get parent directory of the current file
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+#Provide CSV file name 
+csv_file = input("Enter the name of the CSV file: ")
+
+result = [] 
+path = []
+for root, dir, files in os.walk(parent_dir):
+    if csv_file in files:
+        result.append(os.path.join(root, csv_file))
+        path = root
+
+csv_file = result[0]
+
+if result is None: 
+    logger.error("File not found")
+    exit(1)
+
+csv_name = (csv_file.split(".")[-2]).split("/")[-1]
+captured_data_file = csv_name + ".ttl"
+captured_data_file = os.path.join(path, captured_data_file)
+
 
 # Define the namespace to connect with the ontology. 
 ns = Namespace("http://test.org/vehicleOntology#")
@@ -22,6 +46,8 @@ graph.add((converted_class, RDF.type, OWL.Class))
 #Define the Data Properties. 
 hasHeartRateThreshold = URIRef(ns["hasHeartRateThreshold"])
 hasOxygenLevelThreshold = URIRef(ns["hasOxygenLevelThreshold"])
+hasResponeRateThreshold = URIRef(ns["hasResponeRateThreshold"])
+hasHeartRateVariabilityThreshold = URIRef(ns["hasHeartRateVariabilityThreshold"])
 
 #Add the datatype properties to the ontology
 graph.add((hasHeartRateThreshold, RDF.type, OWL.DatatypeProperty))
@@ -30,10 +56,19 @@ graph.add((hasHeartRateThreshold, RDFS.range, XSD.float))
 
 graph.add((hasOxygenLevelThreshold, RDF.type, OWL.DatatypeProperty))
 graph.add((hasOxygenLevelThreshold, RDFS.domain, converted_class))
-graph.add((hasOxygenLevelThreshold, RDFS.range, XSD.float))
+graph.add((hasOxygenLevelThreshold, RDFS.range, XSD.int))
 
-logging.debug("Class and Data Properties added to the ontology.")
-with open('../data_SAT_20240603.csv', 'r') as file: #Give the /path/to/your/csv/file.csv 
+graph.add((hasResponeRateThreshold, RDF.type, OWL.DatatypeProperty))
+graph.add((hasResponeRateThreshold, RDFS.domain, converted_class))
+graph.add((hasResponeRateThreshold, RDFS.range, XSD.float))
+
+graph.add((hasHeartRateVariabilityThreshold, RDF.type, OWL.DatatypeProperty))
+graph.add((hasHeartRateVariabilityThreshold, RDFS.domain, converted_class))
+graph.add((hasHeartRateVariabilityThreshold, RDFS.range, XSD.float))
+
+logger.debug("Class and Data Properties added to the ontology.")
+
+with open(csv_file, 'r') as file: #Give the /path/to/your/csv/file.csv 
     reader = csv.DictReader(file)
     for index, row in enumerate(reader):
 
@@ -44,11 +79,20 @@ with open('../data_SAT_20240603.csv', 'r') as file: #Give the /path/to/your/csv/
         graph.add((threshold_uri, RDF.type, converted_class))
 
         #Adding them to the new ontology. 
-        graph.add((threshold_uri, ns.hasHeartRateThreshold, Literal(row["HR"], datatype=XSD.float)))
-        graph.add((threshold_uri, ns.hasOxygenLevelThreshold, Literal(row["RR"], datatype=XSD.float)))
+        graph.add((threshold_uri, ns.hasHeartRateThreshold, Literal(row["Maximum-HR"], datatype=XSD.float)))
+        graph.add((threshold_uri, ns.hasHeartRateThreshold, Literal(row["Minimum-HR"], datatype=XSD.float)))
+        
+        graph.add((threshold_uri, ns.hasResponeRateThreshold, Literal(row["Maximum-RR"], datatype=XSD.float)))
+        graph.add((threshold_uri, ns.hasResponeRateThreshold, Literal(row["Minimum-RR"], datatype=XSD.float)))
+        
+        graph.add((threshold_uri, ns.hasHeartRateVariabilityThreshold, Literal(row["Maximum-HRV"], datatype=XSD.float)))
+        graph.add((threshold_uri, ns.hasHeartRateVariabilityThreshold, Literal(row["Minimum-HRV"], datatype=XSD.float)))
+        
+        graph.add((threshold_uri, ns.hasOxygenLevelThreshold, Literal(row["Maximum-SPO2"], datatype=XSD.int)))
+        graph.add((threshold_uri, ns.hasOxygenLevelThreshold, Literal(row["Minimum-SPO2"], datatype=XSD.int)))
         
         # Add other properties as needed
-
+ 
 # Serialize the graph to an RDF file or load directly into Protege
-graph.serialize("../thresholds.ttl")
-logging.info("Conversion completed successfully.")
+graph.serialize(captured_data_file)
+logger.info("Conversion completed successfully.")
